c900d6e085b1ee3796e8fb8d10269c7ecd353e2e
GowthamA1963 <aa8366@srmist.edu.in>
2026-01-01 16:44:09 +0530

    Remove build artifacts from repository

--- build/rmf_demos_panel/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/ros2/rmf_ws/install/rmf_demos_dashboard_resources/share/rmf_demos_dashboard_resources/package.sh"
--- build/rmf_demos_panel/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ros2/rmf_ws/install/rmf_demos_panel'
